abba81b1b1ff466e804cab0ed09898a59923aac9
Neelu <neelu.rsa@gmail.com>
2020-04-25 09:04:19 +0100

    Disable strict slashes

--- myranker/__init__.py
+++ myranker/__init__.py
@@ -10,6 +10,8 @@ def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
 
+    app.url_map.strict_slashes = False
+
     db.init_app(app)
 
     from myranker.main.routes import main
